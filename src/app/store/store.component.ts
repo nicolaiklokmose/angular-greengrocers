4f34aa965262d7215fdce1fb2b663b3590c097e7
Nicolai Klokmose <angryniczorn@gmail.com>
2024-03-14 17:38:21 +0100

    extension done

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { GreenGrocersService } from '../services/greengrocers.service';
 import { Item } from '../models/item';
+import { map } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-store',
@@ -25,5 +27,30 @@ export class StoreComponent implements OnInit {
   addToCart(item: Item): void {
     item.quantity = 1;
     this.greenGrocersService.addToCart(item);
+  }
+
+  /** EXTENSION FUNCTIONS */
+  filterByType(type: string) {
+    console.log("filter test")
+    this.showAll();
+    this.items$ = this.items$.pipe(
+      map(items => items.filter((item: Item) => item.type === type))
+    );
+  }
+
+  sortByPrice() {
+    this.items$ = this.items$.pipe(
+      map(items => items.sort((a, b) => a.price - b.price))
+    );
+  }
+
+  sortByName() {
+    this.items$ = this.items$.pipe(
+      map(items => items.sort((a, b) => a.name.localeCompare(b.name)))
+    );
+  }
+  
+  showAll() {
+    this.items$ = this.greenGrocersService.getItems();
   }
 }
